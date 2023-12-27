5e3e0742035862de6864e13188c0c7cbdffe4c61
NilletGamboa <gamboa.nillet@gmail.com>
2023-12-26 21:41:12 -0500

    26/12/2023 20:41

---
+++ src/main/java/com/example/springrest/controllers/dto/ProductDTO.java
@@ -0,0 +1,26 @@
+package com.example.springrest.controllers.dto;
+
+import com.example.springrest.entities.Maker;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProductDTO {
+    private Long id;
+
+    private String name;
+
+    private BigDecimal price;
+
+    private Maker maker;
+}
